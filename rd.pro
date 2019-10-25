186b66f9ee0acdc469cf2bf7ce47d72ac9f19fec
ivansstyle <ivansstyle@me.com>
2019-10-25 18:00:04 +0100

    Again?

---
+++ rd.pro
@@ -0,0 +1,11 @@
+TARGET=rd
+
+
+#Compiler settings
+#QMAKE_CXXFLAGS +=  -Wall -O3 -ffast-math -fopenmp
+#QMAKE_LFLAGS += -fopenmp
+CONFIG+=C++14
+
+
+SOURCES += \
+    src/main.cpp
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+// This is a main file
+// Provides main routine
+
+#include <iostream>
+#include <vector>
+
+int main(int argc,char *argv[])
+{
+    // Checking if there are any arguments
+    std::vector<std::string> args;
+
+    for (int a=0; a < argc; ++a) {
+            args.push_back(argv[a]);
+    }
+
+
+
+
+} // End of program
