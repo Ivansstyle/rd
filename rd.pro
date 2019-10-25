88ad8f05c5663092838616a7e3388945dbfc78f0
ivansstyle <ivansstyle@me.com>
2019-10-25 18:28:42 +0100

    Working main

--- rd.pro
+++ rd.pro
@@ -2,10 +2,14 @@ TARGET=rd
 
 
 #Compiler settings
-#QMAKE_CXXFLAGS +=  -Wall -O3 -ffast-math -fopenmp
+QMAKE_CXXFLAGS +=  -Wall -O3 -ffast-math -fopenmp
 #QMAKE_LFLAGS += -fopenmp
 CONFIG+=C++14
 
 
 SOURCES += \
     src/main.cpp
+
+
+#Defines
+DEFINES += DEBUG
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
-// This is a main file
+// main.cpp
 // Provides main routine
 
+/// @author Ivans Saponenko
+/// @macro iDEBUG = self-defined debug macro
+
+// Includes
 #include <iostream>
 #include <vector>
+
+
+// Define DEBUG to print debug info to the terminal
+#ifndef iDEBUG
+#define iDEBUG
+#endif
+
+#ifdef iDEBUG
+void onelineprint(std::string s)
+{
+    std::cout<< s << " \n";
+}
+#endif
+
+
 
 int main(int argc,char *argv[])
 {
@@ -12,8 +31,15 @@ int main(int argc,char *argv[])
     for (int a=0; a < argc; ++a) {
             args.push_back(argv[a]);
     }
+#ifdef iDEBUG //Printing received arguments in the defined function
+    onelineprint("\nPassed in arguments:");
+    for (std::string arg : args) {
+        std::cout<< arg << "; ";
+    }
+    onelineprint("\nNo more arguments passed");
+#endif
 
-
-
-
+    // Setting filepaths
+    std::string dir = args[0];
+    std::string dist = args[1];
 } // End of program
