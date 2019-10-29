9095aaabcaafaae36bae40cf5d4fc103ab789a47
ivansstyle <ivansstyle@me.com>
2019-10-29 15:55:33 +0000

    Updated running environment. Fixed the directory bug, now all should be good

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #include <chrono> ///@TODO Set-up application timing
 
 
-// Define DEBUG to print debug info to the terminal
+// Define iDEBUG to print debug info to the terminal
 #ifndef iDEBUG
 #define iDEBUG
 #endif
@@ -23,18 +23,30 @@ void onelineprint(std::string s)
 }
 #endif
 
+// Define the output file extention
+#define OUTPUT_EXTENTION "txt"
+#define PROGRAM_NAME "rd"
+#define DEFAULT_FILE_NAME "unnamed"
+
+
 // Defining globals
 static std::string dir;
 static std::string dist;
+static std::string filepath;
+static std::string program_name;
 
 int main(int argc,char *argv[])
 {
+    // Getting a name
+    program_name = PROGRAM_NAME;
+
     // Reading the arguments
     std::vector<std::string> args;
 
     for (int a=0; a < argc; ++a) {
             args.push_back(argv[a]);
     }
+
 #ifdef iDEBUG // @DEBUG Printing received arguments
     onelineprint("\nPassed in arguments:");
     for (std::string arg : args) {
@@ -45,6 +57,52 @@ int main(int argc,char *argv[])
 
     // Setting filepaths
     dir = args[0];
+
+    { // Deleting the program name from distination
+        size_t strsize = dir.size();
+        size_t namesize;
+        std::string n = std::string(OUTPUT_EXTENTION);
+        namesize = n.size();
+        dir.resize(strsize - namesize);
+    } // works
+
     dist = args[1];
 
+    int idx_scanargs = 2; // Set a argument scan index to 2, 0 - filepath, 1 - output file
+    // If output file is not specified, set scan to start with idx[1]
+
+    char compare = dist[0];
+    if(compare == 45) // Check for the output file specification, 45 = "-" in ascii
+        // @TODO Change to more versatile scanner
+    {
+        std::cout<<"Output file has not been specified"<<"\n";
+        std::cout<<"Setting default output destination to "<<
+                   dir + "/" + DEFAULT_FILE_NAME + "." + OUTPUT_EXTENTION <<
+                   "\n";
+
+        dist = std::string(DEFAULT_FILE_NAME)+ "." + OUTPUT_EXTENTION;
+        filepath = dir + dist;
+    }
+
+    std::cout<<dir<<std::endl;
+
+#ifdef iDEBUG // @DEBUG Printing received arguments
+    onelineprint("Working directory and distination directory");
+    onelineprint("DIR: "+dir);
+    onelineprint("DIST: "+dist);
+    onelineprint("FILEPATH: "+filepath);
+#endif
+
+
+
+    int running = 0;
+    // Load and initialize
+
+    // Running loop
+    while(running)
+    {
+
+    }
+
+    return EXIT_SUCCESS;
 } // End of program
